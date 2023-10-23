406bb97ded317bcd4c40fa3adf49c283ccd157c5
Jordan H <jordan@hrvy.uk>
2023-10-23 17:12:25 +0100

    (core) handle exc under _get_devices (core) add create method to remove second call to update

--- pynintendoparental/__init__.py
+++ pynintendoparental/__init__.py
@@ -1,3 +1,4 @@
+# pylint: disable=broad-exception-caught
 """The core Python module."""
 
 from .authenticator import Authenticator
@@ -10,8 +11,8 @@ class NintendoParental:
 
     def __init__(self,
                  auth: Authenticator,
-                 timezone: str = "Europe/London",
-                 lang: str = "en-GB") -> None:
+                 timezone,
+                 lang) -> None:
         self._api: Api = Api(auth=auth, tz=timezone, lang=lang)
         self.account_id = auth.account_id
         self.devices: list[Device] = None
@@ -22,7 +23,12 @@ class NintendoParental:
             endpoint="get_account_devices",
             ACCOUNT_ID=self.account_id
         )
-        self.devices = await Device.from_devices_response(response["json"], self._api)
+
+        try:
+            self.devices = await Device.from_devices_response(response["json"], self._api)
+        except Exception as err:
+            self.devices = []
+            raise RuntimeError(err) from err
         _LOGGER.debug("Found %s device(s)", len(self.devices))
 
     async def update(self):
@@ -30,3 +36,13 @@ class NintendoParental:
         _LOGGER.debug("Received request to update data.")
         await self._get_devices()
         _LOGGER.debug("Update complete.")
+
+    @classmethod
+    async def create(cls,
+                 auth: Authenticator,
+                 timezone: str = "Europe/London",
+                 lang: str = "en-GB") -> 'NintendoParental':
+        """Create an instance of NintendoParental."""
+        self = cls(auth, timezone, lang)
+        await self.update()
+        return self
