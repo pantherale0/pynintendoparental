d9339d8dfa4c16b1ba78c70654eb8e36e9579d02
Jordan <jordan@hrvy.uk>
2024-01-14 15:04:34 +0000

    0.4.1
    fix eachDayOfTheWeekRegulations error

--- pynintendoparental/device.py
+++ pynintendoparental/device.py
@@ -146,7 +146,7 @@ class Device:
 
     def _update_day_of_week_regulations(self):
         """Override the limit / bed time for the device from parental_control_settings if individual days are configured."""
-        day_of_week_regs = self.parental_control_settings["playTimerRegulations"]["dailyRegulations"]["eachDayOfTheWeekRegulations"]
+        day_of_week_regs = self.parental_control_settings["playTimerRegulations"]["eachDayOfTheWeekRegulations"]
         current_day = day_of_week_regs[DAYS_OF_WEEK[datetime.now().weekday()]]
         if current_day["timeToPlayInOneDay"]["enabled"]:
             self.limit_time = current_day["timeToPlayInOneDay"]["limitTime"]
