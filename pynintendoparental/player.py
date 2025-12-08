b92a8b6cc2e663c6719b8db4cb1e2dee925b7778
Jordan Harvey <jordan@hrvy.uk>
2025-12-08 23:24:06 +0000

    update player parser

--- pynintendoparental/player.py
+++ pynintendoparental/player.py
@@ -18,8 +18,7 @@ class Player:
         """Update the current instance of the player from the daily summery"""
         _LOGGER.debug("Updating player %s daily summary", self.player_id)
         for player in raw[0].get("players", []):
-            if self.player_id is player["profile"].get("playerId"):
-                self.player_id = player["profile"].get("playerId")
+            if self.player_id == player["profile"].get("playerId"):
                 self.player_image = player["profile"].get("imageUri")
                 self.nickname = player["profile"].get("nickname")
                 self.playing_time = player.get("playingTime")
